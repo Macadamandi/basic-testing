f96f06516200a81e94734c0f17c1088794b7b8b4
Macadamandi <vlad.yurchenko.2024@gmail.com>
2025-11-05 21:32:34 +0200

    test: snapshot testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,35 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const values = [1, 2, 3];
+    const linkedList = generateLinkedList(values);
+
+    // expected structure
+    const expected = {
+      value: 1,
+      next: {
+        value: 2,
+        next: {
+          value: 3,
+          next: {
+            value: null,
+            next: null,
+          },
+        },
+      },
+    };
+
+    expect(linkedList).toStrictEqual(expected);
   });
 
-  // Check match by comparison with snapshot
+  // match comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const values = ['a', 'b', 'c'];
+    const linkedList = generateLinkedList(values);
+
+    // Snapshot testing: Jest will save this object structure
+    // in a __snapshots__ folder the first time
+    expect(linkedList).toMatchSnapshot();
   });
 });
